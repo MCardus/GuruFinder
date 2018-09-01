5411966b178fd966b5006ba9800db63fea0281d1
MCardus <mcardusg@gmail.com>
2018-09-01 17:03:37 +0200

    fixing bugs in doc2vec

--- gururecommender/guru_recommender.py
+++ gururecommender/guru_recommender.py
@@ -16,18 +16,20 @@ import ast
 
 class GuruRecommender(object):
 
-    def __init__(self, config=None,
+    def __init__(self,
+                 config=None,
                  default_lda_pickle_filepath="gururecommender/models/lda.pkl",
                  default_vectorizer_pickle_filepath="gururecommender/models/vectorizer.pkl",
-                 default_model_pickle_filepath="gururecommender/models/d2v.model"):
+                 default_doc2vec_pickle_filepath="gururecommender/models/d2v.model"):
+
         if not config:
             with open('gururecommender/conf/config.json', 'r') as f:
                 config = json.load(f)
 
         self.elasticsearch = ElasticsearcCli(host=config["elastic_host"])
-        self.lda = LDA(default_lda_pickle_filepath,
-                       default_vectorizer_pickle_filepath)
-        self.doc2vec = Doc2Vec(default_model_pickle_filepath)
+        self.lda = LDA(default_lda_pickle_filepath=default_lda_pickle_filepath,
+                       default_vectorizer_pickle_filepath=default_vectorizer_pickle_filepath)
+        self.doc2vec = Doc2Vec(default_doc2vec_pickle_filepath=default_doc2vec_pickle_filepath)
         self.LDA_MODEL = "lda"
         self.DOC2VEC_MODEL = "doc2vec"
         self.WORD2VEC_MODEL = "word2vec"
@@ -40,39 +42,60 @@ class GuruRecommender(object):
         logging.info(f"""Elastic top n: {elastic_top_n}""")
         return elastic_top_n
 
-    def top_n_gurus(self, input_text, model_type, n=10):
+    def top_n_gurus(self, input_text, model_type, n=10, elastic_filter=True):
         logging.info(f"""Looking for top {n} gurus using model type {model_type}""")
-        # Elasticsearch pre-selection
-        first_selection_gurus = self.elasticsearch_top_n_gurus(input_text=input_text,
-                                                               n=n*2)
-        first_selection_guru_codes = self.elasticsearch.retrieve_gurus_codes(first_selection_gurus, model_type=model_type)
-        codes = np.array([ast.literal_eval(string_code) for string_code in list(first_selection_guru_codes.values())])
+        logging.info(f"""Elastic filter {elastic_filter}""")
+        if elastic_filter:
+            logging.info(f"""Elasticsearch is looking for similar documents to: {input_text}""")
+            # Elasticsearch first layer recommendation
+            first_selection_gurus = self.elasticsearch_top_n_gurus(input_text=input_text, n=n*2)
+            if len(first_selection_gurus) == 0:
+                raise ValueError("Zero Gurus retrieved on first recommendation layer")
+            else:
+                logging.info(f"""Retrieved {len(first_selection_gurus)} gurus on first recommendation layer""")
+        else:
+            # Obtaining all gurus since there is no prior filter
+            logging.info("Reetrieving all gurus names")
+            first_selection_gurus = self.elasticsearch.retrieve_gurus_names()
+
+        # Retrieving text model codes
+        seconds_selection_codes = self.elasticsearch.retrieve_gurus_codes(first_selection_gurus, model_type=model_type)
+        clean_codes = np.array([ast.literal_eval(string_code)
+                                for string_code in list(seconds_selection_codes.values())])
 
         # Creating input_text code
         input_text_code = self._predict_single(input_text=input_text, model_type=model_type)
 
         # Looking for nearest gurus
-        tree = BallTree(codes, leaf_size=len(codes)*2)
-        k=len(codes) if n > len(codes) else n
+        tree = BallTree(clean_codes, leaf_size=len(clean_codes)*2)
+        k=len(clean_codes) if n > len(clean_codes) else n
         dist, ind = tree.query(input_text_code, k=k)
-        top_n_gurus = [list(first_selection_guru_codes.keys())[index] for index in ind[0]]
+        top_n_gurus = [list(seconds_selection_codes.keys())[index] for index in ind[0]]
         logging.info(f"""Top n gurus: {top_n_gurus}""")
         return top_n_gurus
 
 
-    def fit(self, model_type, lda_topic_granurality=3, lda_max_topics=12):
+    def fit(self, model_type,
+            lda_topic_granurality=3,
+            lda_max_topics=12,
+            words_min_freq=0.01,
+            words_max_freq=0.8,
+            doc2vec_workers=10):
         """
         Collect all tweets in elastic and trains a new model which is serialized in disk
         :param model_type: Model type
         """
         logging.info(f"""Fit using model type {model_type}""")
         tweets_list = list(self.elasticsearch.retrieve_all_tweets().values())
-        tweets_list_processed = [". ".join(tweet) for tweet in tweets_list]
+        # Generating flat list
+        tweets_list_processed = np.concatenate(tweets_list).ravel().tolist()
         if model_type.lower() == self.LDA_MODEL:
             logging.info(f"""LDA fit in proces""")
             self.lda.fit(tweets_list_processed,
                          topic_search_granurality=lda_topic_granurality,
-                         max_topics=lda_max_topics)
+                         max_topics=lda_max_topics,
+                         words_min_freq=words_min_freq,
+                         words_max_freq=words_max_freq)
         elif model_type.lower() == self.WORD2VEC_MODEL:
             pass
         elif model_type.lower() == self.DOC2VEC_MODEL:
@@ -101,7 +124,7 @@ class GuruRecommender(object):
         if model_type.lower() == self.LDA_MODEL:
             prediction = self.lda.predict(np.array([input_text]))
         elif model_type.lower() == self.DOC2VEC_MODEL:
-            self.doc2vec.predict(np.array([input_text]))
+            prediction = np.array([self.doc2vec.predict(input_text)])
         else:
             raise ValueError(f"""Model {model_type} does not exists""")
         return prediction
@@ -120,10 +143,27 @@ if __name__ == "__main__":
                     "--input",
                     required=False,
                     help="input recommendation text")
+    ap.add_argument("-n",
+                    "--num_recommendation",
+                    required=False,
+                    default=15,
+                    help="num of recommended gurus")
+    ap.add_argument("-e",
+                    "--elastic_filter",
+                    required=False,
+                    default="y",
+                    help="Elastic filter. Options: y,n")
     args = vars(ap.parse_args())
     action = args['action']
     model = args['model']
     input_text = str(args['input'])
+    elastic_filter = args['elastic_filter']
+    elastic_filter = False if elastic_filter == "n" else True
+    try:
+        num_recommendation = int(args['num_recommendation'])
+    except:
+        logging.warning(f"""Num of recomendations {args['num_recommendation']} should be an integer""")
+        num_recommendation=0
     gurufinder = GuruRecommender()
 
     if not model in [gurufinder.DOC2VEC_MODEL, gurufinder.LDA_MODEL]:
@@ -133,7 +173,7 @@ if __name__ == "__main__":
     elif action == "predict":
             gurufinder.predict(model_type=model)
     elif action == "recommend":
-            gurufinder.top_n_gurus(input_text=input_text,model_type=model)
+            gurufinder.top_n_gurus(input_text=input_text, model_type=model, n=num_recommendation, elastic_filter=elastic_filter)
     else:
         raise ValueError(f"""action {action} not known""")
     # while True:
--- test/test_guru_recommender.py
+++ test/test_guru_recommender.py
@@ -9,32 +9,43 @@ class TestGuruRecommender(unittest.TestCase):
         config = {'elastic_host': 'localhost', 'port': 9200}
         self.guru_recommender = \
             GuruRecommender(config=config,
-                            default_model_pickle_filepath="models/d2v.model",
-                            default_lda_pickle_filepath="gururecommender/models/lda.pkl",
-                            default_vectorizer_pickle_filepath="gururecommender/models/vectorizer.pkl")
+                            default_doc2vec_pickle_filepath="models/d2v.model",
+                            default_lda_pickle_filepath="models/lda.pkl",
+                            default_vectorizer_pickle_filepath="models/vectorizer.pkl")
 
     def test_lda_fit(self):
         # assert void method, and no exception is raised
-        self.assertEqual(self.guru_recommender.fit(model_type=self.guru_recommender.LDA_MODEL), None)
+        self.assertEqual(self.guru_recommender.fit(model_type=self.guru_recommender.LDA_MODEL,
+                                                   words_min_freq=0.0001,
+                                                   words_max_freq=0.7), None)
 
     def test_lda_predict(self):
         # assert void method, and no exception is raised
         self.assertEqual(self.guru_recommender.predict(model_type=self.guru_recommender.LDA_MODEL),None)
 
+    def test_top_n_gurus_lda(self):
+        input_text = "I made these this morning, first time. Followed recipe exactly and they were Perfect! All ingredients pulsed in a food processor. My batter was thick enough in about 4 minutes. I used a non stick griddle, buttered. The heat setting is medium low to low. Make sure griddle is hot before cooking. 4 minutes first side, 3 minutes second side. Low and slow is the correct way to cook these. No sticking whatsoever. As an aside, my psyllium was orange flavored Metamucil, so I sprinkled in about a teaspoon of unsweetened coconut flakes and called them Tropical pancakes with the hint of orange undertone!"
+        output = self.guru_recommender.top_n_gurus(input_text=input_text, model_type='lda', n=3)
+        assert isinstance(output,list)
+
     def test_doc2vec_fit(self):
         # assert void method, and no exception is raised
-        self.guru_recommender.fit(model_type=self.guru_recommender.DOC2VEC_MODEL)
+        self.assertEqual(self.guru_recommender.fit(model_type=self.guru_recommender.DOC2VEC_MODEL), None)
 
     def test_doc2vec_predict(self):
         # assert void method, and no exception is raised
         self.guru_recommender.predict(model_type=self.guru_recommender.DOC2VEC_MODEL)
 
-    def test_top_n_gurus_lda(self):
+    def test_top_n_gurus_doc2vec(self):
         input_text = "I made these this morning, first time. Followed recipe exactly and they were Perfect! All ingredients pulsed in a food processor. My batter was thick enough in about 4 minutes. I used a non stick griddle, buttered. The heat setting is medium low to low. Make sure griddle is hot before cooking. 4 minutes first side, 3 minutes second side. Low and slow is the correct way to cook these. No sticking whatsoever. As an aside, my psyllium was orange flavored Metamucil, so I sprinkled in about a teaspoon of unsweetened coconut flakes and called them Tropical pancakes with the hint of orange undertone!"
-        output = self.guru_recommender.top_n_gurus(input_text=input_text,
-                                                   model_type='lda',
-                                                   n=3)
-        print(output)
+        output = self.guru_recommender.top_n_gurus(input_text=input_text, model_type='doc2vec', n=3)
+        assert isinstance(output, list)
+
+    def test_top_n_gurus_doc2vec_no_elastic_filter(self):
+        input_text = "I made these this morning, first time. Followed recipe exactly and they were Perfect! All ingredients pulsed in a food processor. My batter was thick enough in about 4 minutes. I used a non stick griddle, buttered. The heat setting is medium low to low. Make sure griddle is hot before cooking. 4 minutes first side, 3 minutes second side. Low and slow is the correct way to cook these. No sticking whatsoever. As an aside, my psyllium was orange flavored Metamucil, so I sprinkled in about a teaspoon of unsweetened coconut flakes and called them Tropical pancakes with the hint of orange undertone!"
+        output = self.guru_recommender.top_n_gurus(input_text=input_text, model_type='doc2vec', n=3, elastic_filter=False)
+        assert isinstance(output, list)
+
 
 
 if __name__ == '__main__':
