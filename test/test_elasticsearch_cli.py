ba3f9a1114809088fec3ff386c7ce9202f853acf
MCardus <mcardusg@gmail.com>
2018-09-01 22:34:06 +0200

    Updating elastic test

--- test/test_elasticsearch_cli.py
+++ test/test_elasticsearch_cli.py
@@ -13,13 +13,19 @@ class TestElasticSearchCli(unittest.TestCase):
         logging.basicConfig(level=logging.INFO)
 
     def test_retrieve_gurus_tweets(self):
-        assert isinstance(self.elasticsearch_cli.retrieve_gurus_tweets(), dict)
+        output = self.elasticsearch_cli.retrieve_gurus_tweets()
+        total_tweets = len(np.concatenate(list(output.values())).ravel().tolist())
+        logging.info(f"""Total tweets: {total_tweets}""")
+        assert isinstance(output, dict)
 
     def test_retrieve_keywords_tweets(self):
         assert isinstance(self.elasticsearch_cli.retrieve_keywords_tweets(), dict)
 
     def test_retrieve_all_tweets(self):
-        assert isinstance(self.elasticsearch_cli.retrieve_all_tweets(), dict)
+        output = self.elasticsearch_cli.retrieve_all_tweets()
+        total_tweets = len(np.concatenate(list(output.values())).ravel().tolist())
+        logging.info(f"""Total tweets: {total_tweets}""")
+        assert isinstance(output, dict)
 
     def test_search_similar_top_n(self):
         input_text = "I love Art Buckwald’s quote: Dinner is not what you do in the evening before something else. Dinner is the evening. That is what makes Savor the Summit held in Park City each June so much fun.  It is an entire evening of great food, fabulous company and making new friends. Cuisine Unlimited is the only caterer invited. It is one of our favorite events of the year and so happy we have many guests who return year after year."
